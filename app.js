eace2b6320038b83c8f7ae3af2f7d9fa8eb10684
Dev205 <dev205mg@gmail.com>
2025-02-11 19:15:51 +0300

    Function get cookie

---
+++ app.js
@@ -0,0 +1,18 @@
+/**
+ * 
+ * @param {string} name
+ * @returns {string} 
+ */
+function getCookie(name) {
+  const cookies = document.cookie.split('; ');
+  const value = cookies
+    .find(c => c.startsWith(name))
+    ?.split('=')[1];
+  if(value === undefined){
+    return null;
+  }
+
+  return decodeURIComponent(value);
+}
+
+console.log(getCookie('baba'));
